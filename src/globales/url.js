8eb786a4951a0645b1e3775eecee2f0f4a9d276b
Jesus <jesusmarencodev@gmail.com>
2020-11-29 11:18:44 -0500

    subiendo

--- src/globales/url.js
+++ src/globales/url.js
@@ -1,1 +1,2 @@
 export const URL_BASE = "http://vps-9e69eae7.vps.ovh.ca:83/";
+export const URL_TEMP = "http://192.168.20.28:5000/";
--- src/helpers/fetchPetition.js
+++ src/helpers/fetchPetition.js
@@ -1,7 +1,19 @@
-import { URL_BASE } from "../globales/url";
+import { URL_BASE, URL_TEMP } from "../globales/url";
 
 export const fetchPetitionGet = (endpoint, method = 'GET') =>{
   const url = `${URL_BASE}${endpoint}`;
+
+   return fetch(url,{
+    method,
+    headers : {
+      'Content-Type':'application/json'
+    },
+    //body : JSON.stringify(data)
+  });
+}
+
+export const fetchPetitionGetTemp = (endpoint, method = 'GET') =>{
+  const url = `${URL_TEMP}${endpoint}`;
 
    return fetch(url,{
     method,
